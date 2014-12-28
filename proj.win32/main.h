7962f5656cf9692146cc40807337fbabc77a9c03
Seongsik Kim <ian@toysmyth.com>
2014-12-29 08:59:34 +0900

    main.h 적용

--- proj.win32/main.h
+++ proj.win32/main.h
@@ -1,6 +1,13 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#if defined _DEBUG && defined _WIN32
+# define _CRTDBG_MAP_ALLOC
+# include <crtdbg.h>
+# define DEBUG_CLIENTBLOCK   new( _CLIENT_BLOCK, __FILE__, __LINE__)
+# define new DEBUG_CLIENTBLOCK
+#endif
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 // Windows Header Files:
